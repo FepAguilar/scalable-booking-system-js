f5cf9ba38552d1aac585e36142ef629234809d08
Felipe Aguilar <felipeaguilar@Felipes-MacBook-Pro.local>
2025-07-31 21:53:55 -0600

    restore user-service strategy build

--- user-service/src/app.module.ts
+++ user-service/src/app.module.ts
@@ -1,13 +1,8 @@
+// src/app.module.ts
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
-import { UserController } from './user/user.controller';
-import { UserService } from './user/user.service';
 
 @Module({
   imports: [UserModule],
-  controllers: [AppController, UserController],
-  providers: [AppService, UserService],
 })
 export class AppModule {}
--- user-service/src/user/user.module.ts
+++ user-service/src/user/user.module.ts
@@ -1,9 +1,17 @@
+// src/user/user.module.ts
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { PrismaClient } from 'generated/prisma';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService],
+  providers: [
+    UserService,
+    {
+      provide: PrismaClient,
+      useValue: new PrismaClient(),
+    },
+  ],
 })
 export class UserModule {}
